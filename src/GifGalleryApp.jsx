3974c2ba9c88c754a55425c6de2d745ce3d4345b
Roberson Garcia <roberson.gar12@gmail.com>
2023-02-12 16:01:52 -0400

    insert gif names to the list

--- src/GifGalleryApp.jsx
+++ src/GifGalleryApp.jsx
@@ -1,37 +1,43 @@
 import { useState } from "react"
 import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from './components/GifGrid';
 
 export const GifGalleryApp = () => {
 
     const [categories, setCategories] = useState(['Pokemon','Dragon Ball']);
 
-    const onAddCategory = () => {
+    const onAddCategory = (newCategory) => {
         // My first solution xddd
         // const newCategories = categories.map(category => category);
         // newCategories.push('valorant');
 
         // setCategories( () => newCategories );
 
-        setCategories(['valorant',...categories]);
+        // setCategories(['valorant',...categories]);
+
+        // categories.push(newCategory);
+        if( categories.includes(newCategory) )
+            return;
+
+        setCategories([newCategory, ...categories]);
     }
 
     return (
         <>
-            {/* Title */}
             <h1>Gif Gallery App</h1>
 
-            {/* Input */}
-            <AddCategory setCategories={ setCategories }/>
+            <AddCategory 
+                onNewCategory={ value => onAddCategory(value) }
+            />
 
-            {/* Gif List */}
-                <ol>
-                    {/* <li>Abc</li> */}
-                    {categories.map( category => {
-                        return <li key={ category }> {category} </li>
-                    })}
-                </ol>
-
-                {/* Gif Item */}
+            {categories.map( category => 
+                (
+                    <GifGrid 
+                        key={category}    
+                        category={category}
+                    />
+                )
+            )}
         </>
     )
 }
--- src/components/AddCategory.jsx
+++ src/components/AddCategory.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 
-export const AddCategory = ( {setCategories} ) => {
+export const AddCategory = ( {onNewCategory} ) => {
 
     const [inputValue, setInputValue] = useState('');
 
@@ -15,7 +15,8 @@ export const AddCategory = ( {setCategories} ) => {
         if(inputValue.trim().length <= 1) 
             return;
 
-        setCategories(cat => [inputValue,...cat]);
+        // setCategories(cat => [inputValue,...cat]);
+        onNewCategory(inputValue.trim());
 
         setInputValue('');
     }
