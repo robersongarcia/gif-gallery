ecd0a74c973b2a2e9f63cdbb9ea213d94248b1fa
Roberson Garcia <roberson.gar12@gmail.com>
2023-02-12 19:05:48 -0400

    loading message

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,6 +9,10 @@ export const GifGrid = ({ category }) => {
         <>
             <h3>{category}</h3>
 
+            {
+                isLoading && ( <h2>Cargando...</h2> )
+            }
+
             <div className="card-grid">
                 {images.map((image) =>(
                     <GifItem key={image.id} {... image}/>
